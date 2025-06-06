54bffa85fe714dd43d341285edad39414c31d6e8
Patrick <info@patrick-harms.de>
2025-06-06 14:02:35 +0200

    Update and rename Viper-1.lua to Viper.lua

---
+++ COMPLANS/PH_CAU_SANDBOX/Viper.lua
@@ -0,0 +1,32 @@
+{
+  aircraft = "F-16CM", 
+  COM1 = {
+    [1] = { freq = 261.5, color = "Blue 1", name = "MAGIC" },
+    [2] = { freq = 261.6, color = "Blue 2", name = "DARKSTAR" },
+    [3] = { freq = 261.7, color = "Blue 3", name = "OVERLORD" },
+    [4] = { freq = 261.8, color = "Blue 4", name = "WIZZARD" },
+    [5] = { freq = 243, color = "Black", name = "Guard" },
+    [6] = { freq = 262.1, color = "Grey 1", name = "TEXACO-1 (KC135) - 32Y" },
+    [7] = { freq = 262.9, color = "Grey 4", name = "TEXACO-2 (KC135) - 23Y" },
+    [8] = { freq = 351.1, color = "Yellow 1", name = "STRIKE PACKAGE 1" },
+    [9] = { freq = 351.2, color = "Yellow 2", name = "STRIKE PACKAGE 2" },
+    [10] = { freq = 261.1, color = "Green 1", name = "SENAKI GND" },
+    [11] = { freq = 261, color = "Green 2", name = "SENAKI TWR" },
+    [12] = { freq = 261.2, color = "Green 3", name = "SENAKI DEP" },
+    [13] = { freq = 261.3, color = "Green 5", name = "SENAKI ARR" },
+    [14] = { freq = 261.4, color = "Green 6", name = "SENAKI APR" },
+    [16] = { freq = 269.2, color = "Amber 3", name = "VAZIANI GND" },
+    [17] = { freq = 269, color = "Amber 4", name = "VAZIANI TWR" },
+    [18] = { freq = 262.5, color = "Amber 5", name = "KOBULETI GND" },
+    [19] = { freq = 262, color = "Amber 6", name = "KOBULETI TWR" },
+    [20] = { freq = 324.5, color = "White 2", name = "JTAC HAMMER" }
+ COM2 = {
+    [1] = { freq = 140, color = "Red 1", name = "Viper Common" },
+    [2] = { freq = 140.1, color = "Red 2", name = "Viper-1" },
+    [3] = { freq = 140.2, color = "Red 3", name = "Viper-2" },
+    [4] = { freq = 140.3, color = "Red 4", name = "Viper-3" },
+    [5] = { freq = 121.5, color = "Black 2", name = "Guard VHF" },
+    [6] = { freq = 140.4, color = "Red 5", name = "Viper-4" },
+    [7] = { freq = 43, color = "White 1", name = "JTAC AXEMAN" }
+  }
+}
