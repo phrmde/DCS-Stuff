f78b7c7c2c66bbd31374182810cced88454c27c5
Patrick <info@patrick-harms.de>
2025-06-16 09:17:41 +0200

    Create DeckPopulateCat2.lua

---
+++ Missions/Mission-Assets/CarrierDeckScripts/DeckPopulateCat2.lua
@@ -0,0 +1,149 @@
+
+local staticObj = {
+	["name"] = "staticCVac Cat2 F-14-1", -- unit name (Name this something identifiable if you wish to remove it later)
+
+-- Copy and paste over this with the units information
+                                        ["livery_id"] = "vf-102 diamondbacks",
+										["category"] = "Planes",
+										["offsets"] = 
+										{
+											["y"] = -12.863677617083,
+											["angle"] = 2.1839569123121,
+											["x"] = 101.94938980497,
+										}, -- end of ["offsets"]
+										["type"] = "F-14B",
+-- Copy and paste over this with the units information end
+
+-- these can be left as is, but is required.
+	["groupId"] = 1,		-- id's of the group/unit we're spawning (will auto increment if id taken?)
+	["unitId"] = 1,
+	["y"] = 0,			-- The initial location of the unit (required else unit will offset on origin of map)
+	["x"] = 0,			
+	["heading"] = 0,
+	["linkUnit"] = 1, -- This value must be set Via 'shipID = #' where # is the id of the ship you wish to spawn on
+	["linkOffset"] = true,
+	["dead"] = false,
+}
+coalition.addStaticObject(country.id.USA, staticObj) -- makes the object
+
+-- ********************************************************
+
+
+
+local staticObj = {
+	["name"] = "staticCVac Cat2 F-14-2", -- unit name (Name this something identifiable if you wish to remove it later)
+
+-- Copy and paste over this with the units information
+                                        ["livery_id"] = "vf-102 diamondbacks",
+										["category"] = "Planes",
+										["offsets"] = 
+										{
+											["y"] = -11.859908094045,
+											["angle"] = 2.236316789872,
+											["x"] = 114.48741666422,
+										}, -- end of ["offsets"]
+										["type"] = "F-14B",
+-- Copy and paste over this with the units information end
+
+-- these can be left as is, but is required.
+	["groupId"] = 1,		-- id's of the group/unit we're spawning (will auto increment if id taken?)
+	["unitId"] = 1,
+	["y"] = 0,			-- The initial location of the unit (required else unit will offset on origin of map)
+	["x"] = 0,			
+	["heading"] = 0,
+	["linkUnit"] = 1, -- This value must be set Via 'shipID = #' where # is the id of the ship you wish to spawn on
+	["linkOffset"] = true,
+	["dead"] = false,
+    ["hidden"] = true,
+}
+coalition.addStaticObject(country.id.USA, staticObj) -- makes the object
+
+-- ********************************************************
+
+local staticObj = {
+	["name"] = "staticCVac Cat2 Hornet 1", -- unit name (Name this something identifiable if you wish to remove it later)
+
+-- Copy and paste over this with the units information
+                                        ["livery_id"] = "vfa-37",
+										["category"] = "Planes",
+										["offsets"] = 
+										{
+											["y"] = -9.8734178586584,
+											["angle"] = 2.2886766674318,
+											["x"] = 126.90466748268,
+										}, -- end of ["offsets"]
+										["type"] = "FA-18C_hornet",
+-- Copy and paste over this with the units information end
+
+-- these can be left as is, but is required.
+	["groupId"] = 1,		-- id's of the group/unit we're spawning (will auto increment if id taken?)
+	["unitId"] = 1,
+	["y"] = 0,			-- The initial location of the unit (required else unit will offset on origin of map)
+	["x"] = 0,			
+	["heading"] = 0,
+	["linkUnit"] = 1, -- This value must be set Via 'shipID = #' where # is the id of the ship you wish to spawn on
+	["linkOffset"] = true,
+	["dead"] = false,
+    ["hidden"] = true,
+}
+coalition.addStaticObject(country.id.USA, staticObj) -- makes the object
+
+-- ********************************************************
+
+local staticObj = {
+	["name"] = "staticCVac Cat2 Hornet 2", -- unit name (Name this something identifiable if you wish to remove it later)
+
+-- Copy and paste over this with the units information
+                                        ["livery_id"] = "vfa-37",
+										["category"] = "Planes",
+										["offsets"] = 
+										{
+											["y"] = -7.8594699568836,
+											["angle"] = 2.2886766674318,
+											["x"] = 137.92195409471,
+										}, -- end of ["offsets"]
+										["type"] = "FA-18C_hornet",
+-- Copy and paste over this with the units information end
+
+-- these can be left as is, but is required.
+	["groupId"] = 1,		-- id's of the group/unit we're spawning (will auto increment if id taken?)
+	["unitId"] = 1,
+	["y"] = 0,			-- The initial location of the unit (required else unit will offset on origin of map)
+	["x"] = 0,			
+	["heading"] = 0,
+	["linkUnit"] = 1, -- This value must be set Via 'shipID = #' where # is the id of the ship you wish to spawn on
+	["linkOffset"] = true,
+	["dead"] = false,
+    ["hidden"] = true,
+}
+coalition.addStaticObject(country.id.USA, staticObj) -- makes the object
+
+-- ********************************************************
+
+local staticObj = {
+	["name"] = "staticCVac Cat2 Hornet 3", -- unit name (Name this something identifiable if you wish to remove it later)
+
+-- Copy and paste over this with the units information
+                                        ["livery_id"] = "vfa-37",
+										["category"] = "Planes",
+										["offsets"] = 
+										{
+											["y"] = -5.9029632483642,
+											["angle"] = 2.2886766674318,
+											["x"] = 150.22011276255,
+										}, -- end of ["offsets"]
+										["type"] = "FA-18C_hornet",
+-- Copy and paste over this with the units information end
+
+-- these can be left as is, but is required.
+	["groupId"] = 1,		-- id's of the group/unit we're spawning (will auto increment if id taken?)
+	["unitId"] = 1,
+	["y"] = 0,			-- The initial location of the unit (required else unit will offset on origin of map)
+	["x"] = 0,			
+	["heading"] = 0,
+	["linkUnit"] = 1, -- This value must be set Via 'shipID = #' where # is the id of the ship you wish to spawn on
+	["linkOffset"] = true,
+	["dead"] = false,
+    ["hidden"] = true,
+}
+coalition.addStaticObject(country.id.USA, staticObj) -- makes the object
